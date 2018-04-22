200dc6eea8890d314bd527b970e10ddf61d7f7bb
Anushree Bagchi <anushreebagchi4791@gmail.com>
2018-04-22 11:08:39 +0530

    Added descriptive comments.

--- design.js
+++ design.js
@@ -1,11 +1,12 @@
 var boxHeight,boxWidth;
-
+// creating grid box with specifications on clicking submit button
 $("#gridForm").on ('submit',function()
 {
     $("#gridTable").empty();
-    boxHeight=$(".height").val();
-    boxWidth=$(".width").val();
+    boxHeight=$(".height").val(); //recording number of rows selected
+    boxWidth=$(".width").val();  // recording number of columns selected
     var str="";
+    // generating append string
     for(var i=0;i<boxHeight;i++)
     {
        str+="<tr>"
@@ -16,12 +17,12 @@ $("#gridForm").on ('submit',function()
        str+="</tr>";    
     }
     $("#gridTable").append(str);
+// selecting color from color picker and changing background of the selected grid
     $("#gridTable td").on("click",function(){
-        var gridColor=$("#colorPicker").val();
-        $(this).css("background-color",gridColor);
+        var gridColor=$("#colorPicker").val();  // selecting color from color picker
+        $(this).css("background-color",gridColor); //changing background of the grid which is clicked
     })
 });
-
 
 $("#gridForm").submit(function(e) {
     e.preventDefault();
